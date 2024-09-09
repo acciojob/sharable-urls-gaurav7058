9ea26dc987ce0134e429127ee33309503422ace8
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-09-09 13:33:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// your code here
+document.getElementById("button").addEventListener("click",()=>{
+  let Fname=document.getElementById("name").value.trim();
+  let year=document.getElementById("year").value.trim();
+  let url=document.getElementById("url");
+	if(Fname && year){
+    url.innerHTML=`https://localhost:8080/?name=${Fname}&year=${year}`
+  }
+  else if(Fname){
+    url.innerHTML=`https://localhost:8080/?name=${Fname}`
+  }
+  else if(year){
+    url.innerHTML=`https://localhost:8080/?year=${year}`
+  }
+  else{
+    url.innerHTML=`https://localhost:8080/`
+  }
+})
